d8ae1aaf298bc9b34ec150e35f0dd90777d29e90
ocpu <martin.hovre@opencubes.io>
2020-07-16 00:25:08 +0200

    Fixed GenericEvent listeners firing correctly

--- src/main/kotlin/io/opencubes/boxlin/adapter/BoxlinContext.kt
+++ src/main/kotlin/io/opencubes/boxlin/adapter/BoxlinContext.kt
@@ -3,21 +3,65 @@ package io.opencubes.boxlin.adapter
 import net.minecraftforge.common.ForgeConfigSpec
 import net.minecraftforge.eventbus.api.Event
 import net.minecraftforge.eventbus.api.EventPriority
+import net.minecraftforge.eventbus.api.GenericEvent
 import net.minecraftforge.fml.ExtensionPoint
 import net.minecraftforge.fml.ModLoadingContext
 import net.minecraftforge.fml.config.ModConfig
 import java.util.function.Consumer
 import java.util.function.Supplier
+import kotlin.reflect.KClass
+import kotlin.reflect.jvm.reflect
 
 class BoxlinContext(private val container: BoxlinContainer) {
   val eventBus get() = container.eventBus
 
   /**
-   * Add a listener to the mod event bus.
+   * Add a event listener to the mod event bus.
+   * @param E The type of events this listener will receive.
+   * @param listener The event listener function that will be called.
    */
-  @Suppress("RemoveExplicitTypeArguments")
-  inline fun <reified T : Event> addListener(noinline listener: (T) -> Unit) =
-    eventBus.addListener<T>(EventPriority.NORMAL, false, T::class.java, Consumer<T>(listener))
+  inline fun <reified E : Event> addListener(noinline listener: (E) -> Unit) = addListener(E::class.java, listener)
+
+  /**
+   * Add a event listener to the mod event bus.
+   * @param E The type of events this listener will receive.
+   * @param eventClass The event type class.
+   * @param listener The event listener function that will be called.
+   */
+  fun <E : Event> addListener(eventClass: Class<E>, listener: (E) -> Unit) {
+    if (GenericEvent::class.java.isAssignableFrom(eventClass))
+      return addGenericListener(eventClass, listener)
+    eventBus.addListener<E>(EventPriority.NORMAL, false, eventClass, listener)
+  }
+
+  /**
+   * Add a event listener to the mod event bus that handles an event type of [GenericEvent].
+   * @param E The type of events this listener will receive.
+   * @param listener The event listener function that will be called.
+   */
+  inline fun <reified E : Event> addGenericListener(noinline listener: (E) -> Unit) {
+    addGenericListener(E::class.java, listener)
+  }
+
+  /**
+   * Add a event listener to the mod event bus that handles an event type of [GenericEvent].
+   * @param E The type of events this listener will receive.
+   * @param eventClass The event type class.
+   * @param listener The event listener function that will be called.
+   */
+  fun <E : Event> addGenericListener(eventClass: Class<E>, listener: (E) -> Unit) {
+    require(GenericEvent::class.java.isAssignableFrom(eventClass)) {
+      "The event class given to addGenericListener is not of type GenericEvent"
+    }
+    val genericType = (listener.reflect()?.parameters?.get(0)?.type?.arguments?.get(0)?.type?.classifier as? KClass<*>)?.java
+    checkNotNull(genericType) { "Unable to get the generic class of the GenericEvent" }
+
+    val validatingListener = Consumer<E> {
+      if (it is GenericEvent<*> && it.genericType == genericType)
+        listener(it)
+    }
+    eventBus.addListener<E>(EventPriority.NORMAL, false, eventClass, validatingListener)
+  }
 
   /**
    * Your mods' generic instance
